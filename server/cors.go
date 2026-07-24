1bf34426ffae6aa17829eee072e77bd599462c25
ROBBOSoftwareTeamlead <software@robbo.world>
2026-07-24 15:32:04 +0300

    chore(cors): allow Scratch GUI origin http://0.0.0.0:5001

--- server/cors.go
+++ server/cors.go
@@ -22,6 +22,7 @@ var defaultCORSOrigins = []string{
 	"http://scratch-gui.robbo.world",
 	"http://127.0.0.1:5001",
 	"http://localhost:5001",
+	"http://0.0.0.0:5001",
 }
 
 func corsOriginAllowed(origin string) bool {
